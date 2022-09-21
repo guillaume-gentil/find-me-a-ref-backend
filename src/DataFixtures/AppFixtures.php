637c7029cd2279cbfbd59ed8d18b92e7e411b35f
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-09-21 11:54:07 +0200

    code(fixtures) add Provider class for real name in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\DataFixtures\Provider\findMeARefProvider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -10,9 +11,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //add Provider class for real name of club in fixtures
+        $findMeARefProvider = new findMeARefProvider();
+        
         //add Fakerphp in french for fixtures
         $faker = Factory::create('fr_FR');
+
         
+
 
         $manager->flush();
     }
---
+++ src/DataFixtures/Provider/findMeARefProvider.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\DataFixtures\Provider;
+
+class findMeARefProvider
+{
+    
+}
