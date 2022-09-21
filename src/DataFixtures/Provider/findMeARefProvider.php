69c22a2b1cfbeae7263c6ee31d00df76f785bb31
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-09-21 12:06:29 +0200

    code(provider) add 11 club's name and random getter

--- src/DataFixtures/Provider/findMeARefProvider.php
+++ src/DataFixtures/Provider/findMeARefProvider.php
@@ -4,5 +4,25 @@ namespace App\DataFixtures\Provider;
 
 class findMeARefProvider
 {
-    
+    private $club = [
+        "Les Aiglons de Valence",
+        "Les Yeti's de Grenoble",
+        "Les Nounours de Nice",
+        "Les Alligators de Tricastin",
+        "Les primates de Voreppe",
+        "Les Frelons de Varces",
+        "Lyon Roller Hockey",
+        "Seynod Roller Hockey",
+        "Krokos de Nîmes",
+        "Les Cerfs de Thonon",
+        "Les Dauphins de Villard-Bonnot",
+    ];
+
+    /**
+     * return a random club's name
+     */
+    public function getClubName()
+    {
+        return $this->club[array_rand($this->club)];
+    }
 }
