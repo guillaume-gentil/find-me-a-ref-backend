4f098a08d0063851e2b084ce00948bf9f3e4923a
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-09-21 11:48:10 +0200

    code(fixtures) add fakerphp use to appfixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,15 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //add Fakerphp in french for fixtures
+        $faker = Factory::create('fr_FR');
+        
 
         $manager->flush();
     }
