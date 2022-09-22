9e99463fa3ae33f3078b3c51f4a6aa4a7e93ed44
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-09-22 09:05:51 +0200

    code(entity): add use serilalizer for all entities

--- src/Controller/Api/V1/GameController.php
+++ src/Controller/Api/V1/GameController.php
@@ -2,20 +2,27 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @route("/api/v1", name="api_v1")
+ */
 class GameController extends AbstractController
 {
     /**
-     * @Route("/api/v1/game", name="app_api_v1_game")
+     * get game's list
+     * @Route("/games", name="games_get_collection", methods={"GET"})
      */
-    public function index(): JsonResponse
+    public function getGamesCollection(GameRepository $gameRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/V1/GameController.php',
+        $games = $gameRepository->findAll();
+
+        return $this->json(['games' => $games], Response::HTTP_OK, [], [
+            'groups' => 'games_get_collection'
         ]);
     }
 }
