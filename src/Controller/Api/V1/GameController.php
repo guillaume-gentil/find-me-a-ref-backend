6272a8f77f28701f6935e312845fa2577817febf
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-09-22 10:01:04 +0200

    code(denormalizer): add copy of denormalizer for entites connection in post json

--- src/Controller/Api/V1/GameController.php
+++ src/Controller/Api/V1/GameController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @route("/api/v1", name="api_v1")
---
+++ src/Serializer/EntityDenormalizer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Serializer;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+/**
+ * Entity normalizer
+ */
+class EntityDenormalizer implements DenormalizerInterface
+{
+    /** @var EntityManagerInterface **/
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Cette méthode permet de vérifier si le denormalizer s'applique sur la donnée
+     * $data -> ID du genre
+     * $type -> La classe vers laquelle dénormaliser $data
+     * 
+     * @inheritDoc
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return strpos($type, 'App\\Entity\\') === 0 && (is_numeric($data));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        return $this->em->find($class, $data);
+        // EntityManagerInterface::find() fonctionne comme les find() des repos
+    }
+}
