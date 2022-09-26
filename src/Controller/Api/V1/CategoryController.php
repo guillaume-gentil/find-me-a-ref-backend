12118de5a0964879d567ce7f63eef2a48d93b81a
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-09-26 16:41:08 +0200

    code(category controller): add new category controller

---
+++ src/Controller/Api/V1/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/api/v1/category", name="app_api_v1_category")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Api/V1/CategoryController.php',
+        ]);
+    }
+}
