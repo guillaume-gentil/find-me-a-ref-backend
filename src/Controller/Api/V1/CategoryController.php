73044674f05e2a423f0fcfd86f16e65244ad3b55
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-10-06 16:25:06 +0200

    code(category controller): find by id and add method

--- src/Controller/Api/V1/CategoryController.php
+++ src/Controller/Api/V1/CategoryController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @route("/api/v1", name="api_v1")
@@ -37,6 +40,44 @@ class CategoryController extends AbstractController
         if(is_null($category)) {
             return $this->json(['error' => 'Category\'s ID not found !'], Response::HTTP_NOT_FOUND);
         }
+
+        return $this->json($category, Response::HTTP_OK, [], [
+            'groups' => 'games_collection'
+        ]);
+    }
+
+    /**
+     * Add new category
+     * @Route("/categories", name="categories_add", methods={"POST"})
+     * 
+     *@return JsonResponse
+     */
+    public function add(
+        Request $request,
+        SerializerInterface $serializer,
+        CategoryRepository $categoryRepository,
+        ValidatorInterface $validator
+    ): JsonResponse
+    {
+        $json = $request->getContent();
+        $category = $serializer->deserialize($json, Category::class, 'json');
+        $category->setCreatedAt(new \DateTimeImmutable('now'));
+
+        $errors = $validator->validate($category);
+        if (count($errors) > 0) {
+            $cleanErrors = [];
+            /**
+             * @var ConstraintViolation $error
+             */
+            foreach($errors as $error) {
+                $property = $error->getPropertyPath();
+                $message = $error->getMessage();
+                $cleanErrors[$property][] = $message;
+            }
+            return $this->json($cleanErrors , Response::HTTP_UNPROCESSABLE_ENTITY );
+        }
+
+        $categoryRepository->add($category, true);
 
         return $this->json($category, Response::HTTP_OK, [], [
             'groups' => 'games_collection'
