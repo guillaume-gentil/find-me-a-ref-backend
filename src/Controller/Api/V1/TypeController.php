67eff2eddbb5de2e5e2600b6950fb04e90d3a5f5
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-10-06 10:36:01 +0200

    code(type controller): method add new type of game

--- src/Controller/Api/V1/TypeController.php
+++ src/Controller/Api/V1/TypeController.php
@@ -3,12 +3,14 @@
 namespace App\Controller\Api\V1;
 
 use App\Entity\Type;
-use App\Repository\GameRepository;
 use App\Repository\TypeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @route("/api/v1", name="api_v1")
@@ -29,14 +31,50 @@ class TypeController extends AbstractController
 
     /**
      * get type of game by id
+     * 
      * @Route("/types/{id}", name="types_by_id", methods={"GET"} ,requirements={"id"="\d+"})
-     *
      */
     public function getTypeById(Type $type = null ): JsonResponse
     {
         if(is_null($type)) {
             return $this->json(['error' => 'Type\'s ID not found !'], Response::HTTP_NOT_FOUND);
         }
+
+        return $this->json($type, Response::HTTP_OK, [], [
+            'groups' => 'games_collection'
+        ]);
+    }
+
+    /**
+     * Add new type of game
+     *
+     * @Route("/types", name="types_add", methods={"POST"})
+     */
+    public function addType(
+    Request $request,
+    SerializerInterface $serializer,
+    TypeRepository $typeRepository,
+    ValidatorInterface $validator
+    ): JsonResponse
+    {
+        $json = $request->getContent();
+        $type = $serializer->deserialize($json, Type::class, 'json');
+
+        $errors = $validator->validate($type);
+        if (count($errors) > 0) {
+            $cleanErrors = [];
+            /**
+             * @var ConstraintViolation $error
+             */
+            foreach($errors as $error) {
+                $property = $error->getPropertyPath();
+                $message = $error->getMessage();
+                $cleanErrors[$property][] = $message;
+            }
+            return $this->json($cleanErrors , Response::HTTP_UNPROCESSABLE_ENTITY );
+        }
+        $type->setCreatedAt(new \DateTimeImmutable('now'));
+        $typeRepository->add($type, true);
 
         return $this->json($type, Response::HTTP_OK, [], [
             'groups' => 'games_collection'
