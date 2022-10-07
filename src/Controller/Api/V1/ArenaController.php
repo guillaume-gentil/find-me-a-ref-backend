3fc90ceb8cec44321965cdbaf9b58727a933eec5
Arnaud-Joguet <arnaud.joguet@gmail.com>
2022-10-07 17:04:41 +0200

    chore(arena controller)

--- src/Controller/Api/V1/ArenaController.php
+++ src/Controller/Api/V1/ArenaController.php
@@ -14,7 +14,6 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\GeolocationManager;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 /**
  * @route("/api/v1", name="api_v1")
@@ -22,6 +21,7 @@ use Symfony\Component\Serializer\Serializer;
 class ArenaController extends AbstractController
 {
     /**
+     * Get a list of all the Arenas
      * @Route("/arenas", name="arenas", methods={"GET"})
      */
     public function getArenas(ArenaRepository $arenaRepository): JsonResponse
@@ -34,7 +34,7 @@ class ArenaController extends AbstractController
     }
 
     /**
-     * get arena by id
+     * Get arena by id
      * @Route("/arenas/{id}", name="arenas_by_id", methods={"GET"}, requirements={"id"="\d+"})
      * 
      * @return JsonResponse
@@ -62,27 +62,19 @@ class ArenaController extends AbstractController
         GeolocationManager $geolocationManager
     ): JsonResponse
     {
+        // get the new data from the request (JSON)
         $json = $request->getContent();
-
-        // Be careful to receive all the fields of the entity in the JSON file
-        /* 
-            {
-                "name": "my-arena",
-                "address": "147 rue de la chamberlière, 26000, valence, france",
-                "zip_code": "26000",
-            }
-        */
-
-        // TODO create SERVICE/event for automatically add createdAt field.
         $arena = $serializer->deserialize($json, Arena::class, 'json');
 
         // for setting longitude and latitude use custom service from GeolocationManager
         $arena->setLatitude($geolocationManager->useGeocoder($arena->getAddress(), 'lat'));
         $arena->setLongitude($geolocationManager->useGeocoder($arena->getAddress(), 'lng'));
+
+        // initialize the property createdAt
         $arena->setCreatedAt(new \DateTimeImmutable('now'));
 
+        // check the Assert (Entity's constraints)
         $errors = $validator->validate($arena);
-
         if (count($errors) > 0) {
             $cleanErrors = [];
             /**
@@ -95,11 +87,13 @@ class ArenaController extends AbstractController
             }
             return $this->json($cleanErrors , Response::HTTP_UNPROCESSABLE_ENTITY );
         }
-
+        
+        // if all the data are OK => save item in DB
         $manager = $doctrine->getManager();
         $manager->persist($arena);
         $manager->flush();
 
+        // response : return the new Arena object 
         return $this->json($arena, Response::HTTP_CREATED, [], [
             'groups' => 'games_collection'
         ]);
@@ -109,7 +103,6 @@ class ArenaController extends AbstractController
      * Edit an Arena
      *
      * @Route("/arenas/{id}/edit", name="arenas_edit", methods={"GET","PUT"}, requirements={"id"="\d+"})
-     * @return JsonResponse
      */
     public function edit(
         Arena $arena =null,
@@ -120,24 +113,30 @@ class ArenaController extends AbstractController
         GeolocationManager $geolocationManager
     ): JsonResponse
     {
+        // validate the Arena ID sent in URL
         if(is_null($arena)) {
             return $this->json(['error' => 'Arena\'s ID not found !'], Response::HTTP_NOT_FOUND);
         }
-
+        
+        // stock the old address from DB
         $previousAddress = $arena->getAddress();
 
         if($request->isMethod('put')) {
-
+            
+            // get the new data from the request (JSON)
             $json = $request->getContent();
             $arena= $serializer->deserialize($json, Arena::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $arena]);
 
+            // retreive from API the Geocode values only if the address change
             if($arena->getAddress() != $previousAddress) {
                 $arena->setLongitude($geolocationManager->useGeocoder($arena->getAddress(), 'lng'));
                 $arena->setLatitude($geolocationManager->useGeocoder($arena->getAddress(), 'lat'));
             }
-
+            
+            // update the property updatedAt
             $arena->setUpdatedAt(new \DateTimeImmutable('now'));
-
+            
+            // check the Assert (Entity's constraints)
             $errors = $validator->validate($arena);
             if (count($errors) > 0) {
                 $cleanErrors = [];
@@ -153,10 +152,14 @@ class ArenaController extends AbstractController
                 
             }
 
-            $manager = $doctrine->getManager();
-            $manager->flush();
+            // if all data are OK => save changes in DB
+            $doctrine
+                ->getManager()
+                ->flush()
+                ;
         }
 
+        // response : return the new Arena object
         return $this->json($arena, Response::HTTP_OK, [], [
             'groups' => 'games_collection'
         ]);
@@ -171,11 +174,15 @@ class ArenaController extends AbstractController
      */
     public function delete(Arena $arena =null, ArenaRepository $arenaRepository): JsonResponse
     {
+        // validate the Arena ID sent in URL
         if(is_null($arena)) {
             return $this->json(['error' => 'Arena\'s ID not found !'], Response::HTTP_NOT_FOUND);
         }
 
+        // delete the Arena
         $arenaRepository->remove($arena, true);
+
+        // response : return OK code without content
         return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 }
